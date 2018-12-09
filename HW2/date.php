714af3fcc32b09825f22ef967e577254170d91c5
Maryna <marvlarz@gmail.com>
2018-12-09 21:15:57 +0200

    improve date task

--- HW2/date.php
+++ HW2/date.php
@@ -2,3 +2,14 @@
 
 setlocale(LC_TIME, 'en_US');
 echo(strftime("%A, %d %B, %G\n"));
+
+//revisited version that returns the same result:
+
+echo(date("l, d F, o\n"));
+
+/* Write program, that will print time in format:
+Wednesday, 22 November, 2018
+
+one PHP file, that should be rung like
+php date.php
+*/
